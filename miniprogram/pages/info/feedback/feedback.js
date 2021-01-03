1e5b0ae15414b31d98789cdadabbd0c999fcac7e
Zing22 <leezing22@gmail.com>
2021-01-03 17:18:50 +0800

    v1.4.13

--- miniprogram/pages/info/feedback/feedback.js
+++ miniprogram/pages/info/feedback/feedback.js
@@ -14,6 +14,22 @@ Page({
     })
   },
 
+  toNewCat() {
+    const src = "cloud://rel-eeeccf.7265-rel-eeeccf-1258586139/系统/新猫问卷.png";
+    wx.previewImage({
+      urls: [src],
+      success: (res) => {
+        console.log(res);
+      },
+      fail: (res) => {
+        console.log(res);
+      },
+      complete: (res) => {
+        console.log(res);
+      },
+    });
+  },
+
   /**
    * 用户点击右上角分享
    */
