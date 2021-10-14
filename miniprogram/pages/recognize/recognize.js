907c53c5d6d956130d23efbca069637fd768bd63
jxzhn <jxzhn@jxzhn.com>
2021-10-14 20:00:51 +0800

    梳理、优化代码逻辑

--- miniprogram/pages/recognize/recognize.js
+++ miniprogram/pages/recognize/recognize.js
@@ -1,8 +1,8 @@
-// pages/ai/ai.js
+// pages/recognize/recognize.js
 const utils = require('../../utils.js');
-const randomInt = utils.randomInt;
-
 const config = require('../../config.js');
+
+const randomInt = utils.randomInt;
 
 Page({
 
@@ -10,345 +10,217 @@ Page({
    * 页面的初始数据
    */
   data: {
-    lensDirection: 'back',
-    src: false,
-    cameraAuth: true,
+    cameraAuth: false,
+    devicePosition: 'back', // 前置/后置摄像头
+    photoPath: null, // 用户选择的照片的路径
+    catList: [], // 展示的猫猫列表
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {},
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  onLoad() {
     this.checkAuth();
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
   async checkAuth() {
-    const that = this;
-    await wx.getSetting({
-      success(res) {
-        console.log("auth:", res.authSetting)
-        that.setData({
-          cameraAuth: res.authSetting["scope.camera"]
-        })
-      }
-    })
+    let setting = await wx.getSetting();
+    console.log('auth setting:', setting.authSetting);
+    this.setData({
+      cameraAuth: setting.authSetting['scope.camera']
+    });
   },
 
-  reqCamAuth() {
-    const that = this;
-    wx.openSetting({
-      success(res) {
-        // console.log(res.authSetting)
-        if (res.authSetting["scope.camera"] === true) {
-          that.setData({
-            cameraAuth: true
-          })
-        }
-      }
-    })
+  async reqCamAuth() {
+    let setting = await wx.getSetting();
+    if (setting.authSetting['scope.camera'] === false) {
+      // 拒绝过授权，这时无法弹窗唤起授权，只能跳转到设置页面
+      await wx.openSetting();
+    } else {
+      // 未申请过，唤起弹窗授权
+      await wx.authorize({
+        scope: 'scope.camera'
+      });
+    }
+    this.checkAuth(); // 重新检查授权情况
   },
 
   takePhoto() {
-    if (this.data.cameraAuth) {
-      const ctx = wx.createCameraContext()
-      ctx.takePhoto({
-        quality: 'high',
-        success: (res) => {
-          this.setData({
-            src: res.tempImagePath
-          })
-          this.uploadPhoto(res.tempImagePath)
-        }
-      })
+    if (!this.data.cameraAuth) {
+      return;
     }
-  },
-  error(e) {
-    //用户不允许使用摄像头时触发
-    console.log(e.detail)
+    const that = this;
+    const ctx = wx.createCameraContext();
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        that.setData({
+          photoPath: res.tempImagePath
+        });
+        that.recognizePhoto();
+      }
+    });
   },
 
-  uploadPhoto(filePath) {
+  async recognizePhoto() {
+    wx.showLoading({
+      title: '翻阅猫谱中',
+      mask: true
+    });
+    // 压缩图片
+    const compressPhotoPath = await this.compressPhoto();
+    // 调用服务端接口进行识别
     const that = this;
-    wx.getImageInfo({
-      src: filePath,
-      success(res) {
-        that.compressImage('.canvas', 'canvasForCompress', res, 500);
+    wx.uploadFile({
+      filePath: compressPhotoPath,
+      name: 'photo',
+      url: config.recognize_url,
+      timeout: 10 * 1000, // 10s超时
+      success(resp) {
+        const res = JSON.parse(resp.data);
+        if (res.ok === true) {
+          that.loadCatsResult(res.data);
+        } else {
+          console.log('get error from interface:', res);
+          wx.hideLoading();
+          // TODO: 错误提示
+        }
+      },
+      fail(err) {
+        console.log(err);
+        wx.hideLoading();
+        // TODO: 错误提示
       }
     })
   },
 
-  async uploadFile(file){
-    wx.showLoading({
-      title: '上传识别中...',
-      mask: true,
-    })
-
-    wx.uploadFile({
-      filePath: file,
-      name: 'photo',
-      url: config.aiUrl,
-      // formData: formData,
-      // header: header,
-      // timeout: 10000,
-      success: (result) => {
-        console.log("ai result:", result);
-        wx.hideLoading()
-        let res = JSON.parse(result.data) // 返回的data是字符串？
-        if (res.ok === true) {
-          this.loadCatsResult(res)
-        } else {
-          // 无识别结果或上传数据格式有错？区分
-        }
-      },
-      fail: (res) => {
-        wx.hideLoading()
-        console.log("upload fail:",res);
-        wx.showToast({
-          title: '上传失败，重新试试吧',
-          icon: 'error',
-          duration: 2500
-        })
-      },
-      complete: (res) => {
-        // console.log('upload complete',res);
-      },
-    })
+  async compressPhoto() {
+    // 获取图像宽高信息
+    const photoInfo = await wx.getImageInfo({
+      src: this.data.photoPath,
+    });
+    console.log('photo info:', photoInfo);
+    // 使用canvas方法压缩图片
+    const canvasSideLen = 500;
+    const drawRate = Math.max(photoInfo.width, photoInfo.height) / canvasSideLen; // 计算缩放比
+    const drawWidth = photoInfo.width / drawRate;
+    const drawHeight = photoInfo.height / drawRate;
+    const ctx = wx.createCanvasContext('canvasForCompress');
+    ctx.drawImage(photoInfo.path, 0, 0, drawWidth, drawHeight);
+    const compressPhotoPath = await new Promise((resolve, reject) => {
+      ctx.draw(false, () => {
+        wx.canvasToTempFilePath({
+          canvasId: 'canvasForCompress',
+          fileType: 'jpg',
+          success(res) {
+            resolve(res.tempFilePath);
+          },
+          fail(err) {
+            reject(err);
+          }
+        });
+      });
+    });
+    return compressPhotoPath;
   },
 
-  async loadCatsResult(catRes) {
-    wx.showLoading({
-      title: '翻阅猫谱中...',
-      mask: true,
-    })
+  async loadCatsResult(catData) {
     let catList = [];
-    let catData = catRes.data
-    const minProb = 0.001
-    const minNum = 3
-    const maxNum = 5 // 展示多少只/概率？
-    for (let index = 0; index < catData.length; index++) {
-      // 最多五只，最少三只，去除概率太低的
-      if (index < maxNum) {
-        if (catData[index].prob > minProb || index < minNum) {
-          let cat = catData[index]
-          let catInfo = await this.loadCat(cat)
-          catList.push(catInfo)
-        } else {
-          break;
-        }
+    // 最多maxNum只，最少minNum只，去除概率太低的
+    const minProb = 0.001;
+    const minNum = 3;
+    const maxNum = 5;
+    for (let index = 0; index < maxNum; index++) {
+      if (catData[index].prob > minProb || index < minNum) {
+        let cat = catData[index];
+        let catInfo = await this.getCatInfo(cat);
+        catList.push(catInfo);
       } else {
         break;
       }
     }
-    console.log("catList:", catList);
+    console.log("cat list:", catList);
     this.setData({
       catList: catList
-    })
-    wx.hideLoading() // 查找猫猫信息结束
+    });
+    wx.hideLoading();
   },
 
-  async loadCat(cat) {
-    const that = this;
-    let cat_id = cat.catID;
-    let accuracy = cat.prob;
-    let catInfo;
+  async getCatInfo(cat) {
     const db = wx.cloud.database();
-    await db.collection('cat').doc(cat_id).get().then(res => {
-      // console.log('cat info res:', res);
-
-      // res.data.characteristics_string = [(res.data.colour || '') + '猫'].concat(res.data.characteristics || []).join('，');
-      if (res.data.colour) {
-        res.data.characteristics_string = (res.data.colour || '') + '猫';
-      }
-
-      res.data.accuracy = (accuracy * 100).toFixed(1);
-
-      catInfo = res.data;
-    });
-    let photo = await this.loadPhoto(catInfo);
+    const catInfo = (await db.collection('cat').doc(cat.catID).get()).data;
+    catInfo.prob = (cat.prob * 100).toFixed(1);
+    let photo = await this.getCatPhoto(catInfo);
     catInfo.photo = photo;
     return catInfo;
   },
 
-  async loadPhoto(cat) {
+  async getCatPhoto(catInfo) {
     const db = wx.cloud.database();
     const photo = db.collection('photo');
-
-    const qf = {
-      cat_id: cat._id,
+    // 从精选照片里随机挑选一张
+    const query = {
+      cat_id: catInfo._id,
       verified: true,
       best: true
     };
-    var total = cat.photo_count;
+    var total = catInfo.photo_count;
     var index = randomInt(0, total);
-
-    var pho_src = (await photo.where(qf).skip(index).limit(1).get()).data[0];
-    // console.log("load cat Photo:", pho_src);
-    return pho_src;
+    var photoURL = (await photo.where(query).skip(index).limit(1).get()).data[0];
+    return photoURL;
   },
 
   choosePhoto() {
     const that = this;
-    wx.chooseMedia({
+    wx.chooseImage({
       count: 1,
-      mediaType: ['image'],
+      sizeType: ['compressed'],
       sourceType: ['album'],
-      // maxDuration: 30,
-      // camera: 'back',
       success(res) {
-        console.log("chooseMedia", res.tempFiles)
         that.setData({
-          src: res.tempFiles[0].tempFilePath
-        })
-        that.uploadPhoto(res.tempFiles[0].tempFilePath)
+          photoPath: res.tempFilePaths[0]
+        });
+        that.recognizePhoto();
       }
-    })
+    });
   },
 
-  back() {
+  reset() {
     this.setData({
-      src: '',
-      catList: ''
-    })
+      photoPath: null,
+      catList: []
+    });
   },
 
   reverseCamera() {
-    if (this.data.cameraAuth) {
-      if (this.data.lensDirection === 'front') {
-        this.setData({
-          lensDirection: 'back'
-        })
-      } else {
-        this.setData({
-          lensDirection: 'front'
-        })
-      }
+    if (!this.data.cameraAuth) {
+      return;
+    }
+    if (this.data.devicePosition === 'front') {
+      this.setData({
+        devicePosition: 'back'
+      });
+    } else {
+      this.setData({
+        devicePosition: 'front'
+      });
     }
   },
 
   savePhoto() {
     wx.saveImageToPhotosAlbum({
-      filePath: this.data.src,
+      filePath: this.data.photoPath,
       success() {
         wx.showToast({
           title: '保存成功',
           icon: 'success',
-          duration: 8000
-        })
+        });
       }
     })
   },
 
   clickCatCard(e) {
-    const cat_id = e.currentTarget.dataset.cat_id;
-    const detail_url = '/pages/genealogy/detailCat/detailCat';
+    const catId = e.currentTarget.dataset.catId;
     wx.navigateTo({
-      url: detail_url + '?cat_id=' + cat_id,
+      url: '/pages/genealogy/detailCat/detailCat?cat_id=' + catId,
     });
-  },
-
-  //压缩图片
-  async compressImage(canvasClass, canvasID, imgInfo, size) {
-    const canvasMax = 2000; // wxml中正方形画布的尺寸px 
-    const that = this;
-    const origin = imgInfo;
-    console.log("oriImg", origin);
-    const draw_rate = Math.max(origin.width, origin.height) / canvasMax;
-    const draw_width = origin.width / draw_rate;
-    const draw_height = origin.height / draw_rate;
-
-    // 压缩后的大小
-    let comp_width, comp_height;
-    if (origin.width > origin.height) {
-      comp_width = size;
-      comp_height = origin.height / origin.width * size;
-    } else {
-      comp_height = size;
-      comp_width = origin.width / origin.height * size;
-    }
-
-    // 获取canvas节点
-    const query = wx.createSelectorQuery()
-    await query.select(canvasClass).fields({
-      node: true,
-      context: true
-    }).exec(
-      async (res) => {
-        const canvas = res[0].node
-        canvas.height = canvasMax;
-        canvas.width = canvasMax
-        const ctx = canvas.getContext('2d')
-        console.log("canvas&ctx", canvas, ctx);
-
-        let img = canvas.createImage(); // 创建一个图片对象
-        img.onload = async () => {
-          await ctx.drawImage(img, 0, 0, draw_width, draw_height);
-          wx.canvasToTempFilePath({
-            canvas: canvas,
-            width: draw_width,
-            height: draw_height,
-            destWidth: comp_width,
-            destHeight: comp_height,
-            fileType: 'jpg',
-            success: function (res) {
-              console.log("canvasToFile:", res, canvas, img);
-              that.setData({
-                compressedPhoto: res.tempFilePath,
-              });
-              that.uploadFile(res.tempFilePath);  // 压缩成功，上传到服务器
-            }
-          }, that);
-        }
-        img.src = origin.path;
-
-      }
-    )
   },
 
 })
