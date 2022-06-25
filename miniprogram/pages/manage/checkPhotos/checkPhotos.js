42bd186152e866805dcfcbf46977d8ce8f9ff722
WondrousWisdomcard <69002053+WondrousWisdomcard@users.noreply.github.com>
2022-06-25 13:55:46 +0800

    News (#19)
    * 以防不测
    
    * 04271445
    
    * news
    
    * 公告功能视觉优化
    
    * 公告功能视觉优化
    
    * 视觉优化补充
    
    * 梳理原有科普文件、缓存
    
    * 科普图缓存重构
    
    * up
    
    * 公告UI、审核方式优化
    
    Co-authored-by: Jun <jundeer0824@gmail.com>
    Co-authored-by: Zing22 <leezing22@gmail.com>

--- miniprogram/pages/manage/checkPhotos/checkPhotos.js
+++ miniprogram/pages/manage/checkPhotos/checkPhotos.js
@@ -1,7 +1,5 @@
 // 审核照片
 const utils = require('../../../utils.js');
-const regeneratorRuntime = utils.regeneratorRuntime;
-const randomInt = utils.regeneratorRuntime;
 const isManager = utils.isManager;
 
 const msg = require('../../../msg.js');
@@ -11,6 +9,9 @@ const sendVerifyNotice = msg.sendVerifyNotice;
 
 const config = require('../../../config.js');
 const notifyVerifyPhotoTplId = config.msg.notifyVerify.id;
+
+const cache = require('../../../cache.js');
+
 const text_cfg = config.text;
 
 // 准备发送通知的列表，姓名：审核详情
@@ -25,6 +26,7 @@ Page({
     tipText: '正在鉴权...',
     tipBtn: false,
     total: '-',
+    campus_list: [],
   },
 
   /**
@@ -62,35 +64,8 @@ Page({
   onUnload: function () {
     console.log('页面退出');
 
-    // 自动数猫图（5分钟一次）
-    // wx.cloud.callFunction({
-    //   name: "countPhoto",
-    //   success: (res) => {
-    //     console.log('数猫图完成', res.result);
-    //   }
-    // });
-
-    // 后台处理缩略图和水印图（改成5min一次）
-    // wx.cloud.callFunction({
-    //   name: "imProcess",
-    //   data: {
-    //     app_name: text_cfg.app_name
-    //   },
-    //   success: (res) => {
-    //     console.log('图片处理完成', res.result);
-    //   }
-    // });
-
     // 发送审核消息
     sendVerifyNotice(notice_list);
-
-    // 重新计算拍照月榜（改成5min一次）
-    // wx.cloud.callFunction({
-    //   name: "getPhotoRank",
-    //   success: (res) => {
-    //     console.log('拍照月榜完成', res.result);
-    //   }
-    // });
   },
 
   /**
@@ -125,7 +100,7 @@ Page({
         that.setData({
           auth: true
         });
-        that.reload();
+        that.loadAllPhotos();
       } else {
         that.setData({
           tipText: '只有管理员Level-1能进入嗷',
@@ -135,150 +110,88 @@ Page({
       }
     }, 1)
   },
-  async loadPhotos() {
-    const db = wx.cloud.database();
-    var photos = (await db.collection('photo').where({
-      verified: false
-    }).get()).data;
-    for (var ph of photos) {
-      var cat = (await db.collection('cat').doc(ph.cat_id).get()).data;
-      ph.cat = cat
-    }
-    console.log(photos);
-    this.setData({
-      photos: photos
-    });
-    return true;
-  },
 
-  reload() {
-    this.requestSubscribeMessage() //判断是否弹出订阅消息设置
+  async loadAllPhotos() {
     wx.showLoading({
       title: '加载中...',
     });
-    const that = this;
     const db = wx.cloud.database();
-    db.collection('photo').where({
+    // 获取所有照片
+    var total_count = (await db.collection('photo').where({
       verified: false
-    }).count().then(res => {
-      console.log(res);
-      that.setData({
-        total: res.total
-      });
-      that.loadPhotos().then(() => {
-        wx.hideLoading();
-      });
+    }).count()).total;
 
+    var pools = [];
+    for (var i=0; i<total_count; i+=20) {
+      pools.push(db.collection("photo").where({verified: false}).skip(i).limit(20).get());
+    }
+
+    var photos = await Promise.all(pools);
+    // 拼接多个array
+    photos = photos.map(x => x.data);
+    photos = Array.prototype.concat.apply([], photos);
+
+    var campus_list = {};
+    for (var photo of photos) {
+      var cache_key = `cat-${photo.cat_id}`;
+      var cat = cache.getCacheItem(cache_key);
+      if (!cat) {
+        var cat = (await db.collection('cat').doc(photo.cat_id).get()).data;
+        cache.setCacheItem(cache_key, cat, 3);
+      }
+      photo.cat = cat;
+
+      // 分类记录到campus里
+      var campus = cat.campus;
+      if (!campus_list[campus]) {
+        campus_list[campus] = [];
+      }
+      console.log(campus, photo);
+      campus_list[campus].push(photo);
+    }
+    
+    this.setData({
+      campus_list: campus_list,
+    })
+    
+    wx.hideLoading();
+  },
+
+  bindClickCampus(e) {
+    var campus = e.currentTarget.dataset.key;
+    var active_campus = this.data.active_campus;
+    console.log(campus);
+
+    if (active_campus == campus) {
+      return;
+    }
+    
+    this.setData({
+      active_campus: campus
     });
   },
 
   async requestSubscribeMessage() {
-    // if (this.data.total <= 2) { //最多剩余几张照片可更新订阅状态，TODO:待更改
-    if (this.data.total >= 0) { 
-      wx.getSetting({
-        withSubscriptions: true,
-        success: res => {
-          console.log("subscribeSet:", res);
-          if ('subscriptionsSetting' in res) {
-            if (!(notifyVerifyPhotoTplId in res['subscriptionsSetting'])) {
-              // 第一次申请或只点了取消，未永久拒绝也未允许
-              requestNotice('notifyVerify');
-              // console.log("firstRequest");
-            } else if (res.subscriptionsSetting[notifyVerifyPhotoTplId] === 'reject') {
-              // console.log("已拒绝");// 不再请求/重复弹出toast
-            } else if (res.subscriptionsSetting[notifyVerifyPhotoTplId] === 'accept') {
-              console.log('重新请求下个一次性订阅');
-              requestNotice('notifyVerify');
-            }
+    wx.getSetting({
+      withSubscriptions: true,
+      success: res => {
+        console.log("subscribeSet:", res);
+        if ('subscriptionsSetting' in res) {
+          if (!(notifyVerifyPhotoTplId in res['subscriptionsSetting'])) {
+            // 第一次申请或只点了取消，未永久拒绝也未允许
+            requestNotice('notifyVerify');
+            // console.log("firstRequest");
+          } else if (res.subscriptionsSetting[notifyVerifyPhotoTplId] === 'reject') {
+            // console.log("已拒绝");// 不再请求/重复弹出toast
+          } else if (res.subscriptionsSetting[notifyVerifyPhotoTplId] === 'accept') {
+            console.log('重新请求下个一次性订阅');
+            requestNotice('notifyVerify');
           }
         }
-      })
-      // if(subscribeSetting['mainSwitch']){} //本小程序订阅消息总开关
-    }
-  },
-  bindCheck(e) {
-    const photo = e.currentTarget.dataset.photo;
-    const best = e.currentTarget.dataset.best;
-    const that = this;
-    wx.showModal({
-      title: '提示',
-      content: '确定通过审核' + (best ? '+精选' : '') + '？',
-      success(res) {
-        if (res.confirm) {
-          console.log('开始通过');
-          wx.cloud.callFunction({
-            name: "managePhoto",
-            data: {
-              type: "check",
-              photo: photo,
-              best: best
-            }
-          }).then(res => {
-            console.log("审核通过：" + photo._id);
-            console.log(res.data);
-            // 内存记录一下这个操作，用来发通知
-            that.addNotice(photo, true);
-
-            // 直接从列表里去掉这只猫，不完全加载了
-            const photos = that.data.photos;
-            const new_photos = photos.filter((ph, index, arr) => {
-              // 这个photo是用户点击的photo，在上面定义的
-              return ph._id != photo._id;
-            });
-            that.setData({
-              photos: new_photos,
-              total: that.data.total - 1
-            }, () => {
-              wx.showToast({
-                title: '审核通过',
-              });
-            });
-
-          })
-        }
       }
     })
   },
 
-  bindDelete(e) {
-    const photo = e.currentTarget.dataset.photo;
-    const that = this;
-    wx.showModal({
-      title: '提示',
-      content: '确定删除？',
-      success(res) {
-        if (res.confirm) {
-          console.log('开始删除');
-          wx.cloud.callFunction({
-            name: "managePhoto",
-            data: {
-              type: "delete",
-              photo: photo
-            }
-          }).then(res => {
-            console.log("删除照片记录：" + photo._id);
-            // 内存记录一下这个操作，用来发通知
-            that.addNotice(photo, false);
-
-            // 直接从列表里去掉这只猫，不完全加载了
-            const photos = that.data.photos;
-            const new_photos = photos.filter((ph, index, arr) => {
-              // 这个photo是用户点击的photo，在上面定义的
-              return ph._id != photo._id;
-            });
-            that.setData({
-              photos: new_photos,
-              total: that.data.total - 1
-            }, () => {
-              wx.showToast({
-                title: '删除成功',
-              });
-            });
-          })
-        }
-      }
-    })
-  },
   openBigPhoto(e) {
     const pid = e.currentTarget.dataset.pid;
     wx.previewImage({
@@ -308,5 +221,98 @@ Page({
     wx.navigateTo({
       url: '/pages/genealogy/detailCat/detailCat?cat_id=' + cat_id,
     })
+  },
+
+  // 标记照片
+  bindMark(e) {
+    const index = e.currentTarget.dataset.index;
+    var mark_type = e.currentTarget.dataset.type;
+    var active_campus = this.data.active_campus;
+    var photos = this.data.campus_list[active_campus];
+
+    if (photos[index].mark == mark_type) {
+      mark_type = "";  // 反选
+    }
+    this.setData({
+      [`campus_list.${active_campus}[${index}].mark`]: mark_type
+    });
+  },
+
+  // 确定处理
+  bindCheckMulti(e) {
+    var active_campus = this.data.active_campus;
+    var photos = this.data.campus_list[active_campus];
+    var nums = {}, total_num = 0;
+    for (const photo of photos) {
+      if (!photo.mark || photo.mark == "") {
+        continue;
+      }
+      if (!(photo.mark in nums)) {
+        nums[photo.mark] = 0;
+      }
+      nums[photo.mark] ++;
+      total_num ++;
+    }
+
+    if (total_num == 0) {
+      return false;
+    }
+    
+    var that = this;
+    wx.showModal({
+      title: '确定批量审核？',
+      content: `删除${nums['delete'] || 0}张，通过${nums['pass'] || 0}张，精选${nums['best'] || 0}张`,
+      success(res) {
+        if (res.confirm) {
+          console.log('开始通过');
+          that.doCheckMulti();
+        }
+      }
+    })
+  },
+
+  // 开始批量处理
+  doCheckMulti() {
+    var that = this;
+    wx.showLoading({
+      title: '处理中...',
+    })
+    var active_campus = this.data.active_campus;
+    var photos = this.data.campus_list[active_campus];
+    const mask2type = {
+      "delete": "delete",
+      "pass": "check",
+      "best": "check",
+    }
+    var all_queries = [], new_photos = [];
+    for (const photo of photos) {
+      if (!photo.mark || photo.mark == "") {
+        new_photos.push(photo);
+        continue;
+      }
+
+      // 准备数据
+      var data = {
+        type: mask2type[photo.mark],
+        photo: photo,
+        best: photo.mark == "best",
+      }
+
+      all_queries.push(wx.cloud.callFunction({
+        name: "managePhoto",
+        data: data
+      }))
+      that.addNotice(photo, (photo.mark != "delete"));
+    }
+    // 阻塞一下
+    Promise.all(all_queries).then(_ => {
+      that.setData({
+        [`campus_list.${active_campus}`]: new_photos,
+      }, () => {
+        wx.showToast({
+          title: '审核通过',
+        });
+      });
+    });
   },
 })
---
+++ miniprogram/pages/news/detailNews/detailNews.js
@@ -0,0 +1,160 @@
+// pages/news/detailNews/detailNews.js
+const {
+    keys
+} = require('../../../packages/regenerator-runtime/runtime.js');
+const utils = require('../../../utils.js');
+const isManager = utils.isManager;
+const shareTo = utils.shareTo;
+const getCurrentPath = utils.getCurrentPath;
+const formatDate = utils.formatDate;
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        news_id: 0,
+        news: 0,
+        showManager: false,
+        updateRequest: false,
+        err: false,
+        photos_path: [],
+        cover_path: "",
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.setData({
+            news_id: options.news_id
+        })
+        this.loadNews();
+        this.checkAuth();
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+        this.onRefresh();
+    },
+
+    // 下拉刷新
+    onRefresh() {
+        this.setData({
+            updateRequest: true
+        })
+        this.loadNews();
+        const that = this;
+        setTimeout(function () {
+            that.setData({
+                updateRequest: false
+            })
+        }, 1000)
+        wx.stopPullDownRefresh();
+    },
+
+    checkAuth() {
+        const that = this;
+        isManager(function (res) {
+            if (res) {
+                that.setData({
+                    showManager: true
+                });
+            }
+        }, 2)
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+        const pagesStack = getCurrentPages();
+        const path = getCurrentPath(pagesStack);
+        const share_text = `${this.data.news.title}`;
+        console.log(shareTo(share_text, path))
+        return shareTo(share_text, path);
+    },
+
+
+    loadNews() {
+        const that = this;
+        const db = wx.cloud.database();
+        db.collection('news').doc(that.data.news_id).get().then(res => {
+            console.log("News Detail:", res);
+            if (!res.data) {
+              that.setData({
+                  err: true,
+              })
+              return;
+            }
+
+            var news = res.data;
+            news.ddate = formatDate(news.date, "yyyy年MM月dd日 hh:mm:ss");
+            if (news.dateLastModify) {
+              news.ddateLastModify = formatDate(new Date(news.dateLastModify), "yyyy年MM月dd日 hh:mm:ss");
+            }
+            that.setData({
+                news: news,
+                photos_path: news.photosPath,
+                cover_path: news.coverPath,
+            })
+        });
+    },
+
+    previewImg: function (event) {
+        const that = this;
+        console.log("Preveiw Image: ", event);
+        wx.previewImage({
+            current: that.data.photos_path[event.currentTarget.dataset.index],
+            urls: that.data.photos_path
+        })
+    },
+
+
+    modifyNews() {
+        const detail_url = '/pages/news/modifyNews/modifyNews';
+        wx.navigateTo({
+            url: detail_url + '?news_id=' + this.data.news_id,
+        });
+    },
+
+    _doRemove(item_id) {
+      wx.cloud.callFunction({
+        name: "newsOp",
+        data: {
+          type: "delete",
+          item_id: item_id,
+        }
+      }).then((res) => {
+        console.log(res);
+        if (!res) {
+          wx.showToast({
+            icon: 'none',
+            title: '删除失败',
+          });
+          return
+        }
+        setTimeout(wx.navigateBack, 1000);
+      });
+    },
+
+    removeNews() {
+        if (this.data.showManager == false) {
+            return;
+        }
+        
+        var that = this;
+        wx.showModal({
+            content: '确定要删除吗？',
+            success: function (sm) {
+                console.log(sm);
+                if (sm.confirm) {
+                  that._doRemove(that.data.news_id);
+                }
+            }
+        })
+    },
+})
