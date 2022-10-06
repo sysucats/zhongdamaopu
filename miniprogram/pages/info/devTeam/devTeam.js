a4e5690e62606aab752fd68a379e9c9d1de10b36
李煜政 <zingli@liyuzhengdeMacBook-Pro.local>
2022-10-06 20:47:49 +0800

    v1.8.12

--- miniprogram/pages/info/devTeam/devTeam.js
+++ miniprogram/pages/info/devTeam/devTeam.js
@@ -11,6 +11,12 @@ Page({
     text_cfg: text_cfg,
     github_link: "https://github.com/sysucats/zhongdamaopu",
     update_log: [{
+      version: "v1.8.12",
+      content: [
+        "优化下部署流程",
+      ],
+      time: "2022/10/06"
+    }, {
       version: "v1.8.11",
       content: [
         "代码优化请求次数",
