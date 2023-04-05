92402221922f499be6153389d40e29ed43690097
Zing22 <leezing22@gmail.com>
2023-04-05 15:09:14 +0800

    1

--- @laf/functions/initDeploy/index.ts
+++
@@ -1,9 +0,0 @@
-//TODO TODO TODO
-
-import cloud from '@/cloud-sdk'
-
-exports.main = async function (ctx: FunctionContext) {
-  // body, query 为请求参数, auth 是授权对象
-  const { auth, body, query } = ctx
-
-}
