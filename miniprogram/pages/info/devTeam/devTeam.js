767e28d683db7e0489679c94d0c17a83ba4783d7
Zing22 <leezing22@gmail.com>
2024-06-04 08:37:03 +0800

    v1.13.4

--- miniprogram/pages/info/devTeam/devTeam.js
+++ miniprogram/pages/info/devTeam/devTeam.js
@@ -10,6 +10,12 @@ Page({
     text_cfg: text_cfg,
     github_link: "https://github.com/sysucats/zhongdamaopu",
     update_log: [{
+      version: "v1.13.4",
+      content: [
+        "修复用户头像设置问题",
+      ],
+      time: "2024/06/03"
+    }, {
       version: "v1.13.3",
       content: [
         "修复猫猫部分UI问题",
