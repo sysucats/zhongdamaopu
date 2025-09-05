81d908108aaee58dbf8cad0f865483231d2256c4
LightHouse <2750220025@qq.com>
2025-09-06 00:03:17 +0800

    修复徽章bug

--- miniprogram/utils/badge.js
+++ miniprogram/utils/badge.js
@@ -40,7 +40,10 @@ async function loadUserBadge(openid, badgeDefMap, options) {
   const userBadges = (await app.mpServerless.db.collection("badge").find({
     _openid: openid
   })).result;
-  const res = await mergeAndSortBadges(userBadges, badgeDefMap, options);
+
+  // 过滤掉已被赠送出去的勋章
+  const filteredBadges = userBadges.filter(badge => !badge.givenTime);
+  const res = await mergeAndSortBadges(filteredBadges, badgeDefMap, options);
   return res;
 }
 
