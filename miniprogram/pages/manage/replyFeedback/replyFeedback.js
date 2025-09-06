32394ae13bed476c06d9280b9021995fcd803402
LightHouse <131727267+LightHouse-1024@users.noreply.github.com>
2025-09-06 13:03:43 +0800

    0906 self refresh after sub reply (#71)
    * 修复徽章赠送后不删减的BUG
    
    * 修复反馈回复后刷新延时而可能导致同一反馈被多次回复后最新消息无法回复的BUG
    
    * 修复反馈回复后刷新延时而可能导致同一反馈被多次回复后最新消息无法回复的BUG_2
    
    * 修复反馈回复后刷新延时而可能导致同一反馈被多次回复后最新消息无法回复的BUG_3

--- miniprogram/pages/manage/replyFeedback/replyFeedback.js
+++ miniprogram/pages/manage/replyFeedback/replyFeedback.js
@@ -70,10 +70,14 @@ Page({
         title: '正在提交...',
         mask: true
       });
+      console.log("open_id:", this.data.feedback._openid)
+      console.log("feedback_id:",  this.data.feedback._id)
       let res = await sendReplyNotice(this.data.feedback._openid, this.data.feedback._id);
+      console.log("res:", res)
       console.log("[bindReply] - sendReplyNotice res", res);
+      console.log("code:", res.apiResponse.errCode)
       const that = this;
-      if (res.errCode == 0) {
+      if (res.apiResponse.errCode == 0) {
         // 记录一下回复的内容和时间
         await api.curdOp({
           operation: 'update',
@@ -84,6 +88,19 @@ Page({
             replyInfo: submitData.replyInfo,
           }
         });
+
+        // 直接调用标记为已处理函数，避免网络延迟导致的重回复bug   
+        // dataTime : 2025/09/05 20:53 
+        // author: Light_House
+        await this.bindCheck();
+
+        // 发送刷新事件
+        app.globalData.eventBus.$emit('feedbackUpdated', {
+          id: this.data.feedback._id,
+          action: 'reply',
+          replyInfo: submitData.replyInfo
+        });
+
         wx.hideLoading();
         wx.showToast({
           title: '回复成功',
@@ -93,7 +110,7 @@ Page({
             setTimeout(wx.navigateBack, 1000)
           }
         })
-      } else {
+      } else if ((res.apiResponse.errCode == 43101)){
         wx.hideLoading();
         wx.showToast({
           title: '回复失败，这可能是因为对方没有订阅或次数耗尽',
@@ -103,9 +120,36 @@ Page({
             setTimeout(wx.navigateBack, 1500)
           }
         });
+      } else {
+        wx.hideLoading();
+        wx.showToast({
+          title: '回复失败，errCode：' + res.apiResponse.errCode,
+          icon: 'none',
+          duration: 1500,
+          success: () => {
+            setTimeout(wx.navigateBack, 1500)
+          }
+        });
       }
     }
-  }
+  },
 
+  async bindCheck() {
+    const feedback = this.data.feedback;
+    
+    console.log('[bindCheck] - 确认反馈处理');
+    
+    // 更新反馈状态为已处理
+    await api.curdOp({
+      operation: 'update',
+      collection: "feedback",
+      item_id: feedback._id,
+      data: {
+        dealed: true,
+        dealDate: api.getDate()
+      }
+    });
+
+  }
 })
 
