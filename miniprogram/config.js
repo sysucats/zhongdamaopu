952b76ca55223db659bbb350b4a570dcf0f99d00
Zing22 <leezing22@gmail.com>
2025-11-02 21:01:30 +0800

    v1.17.2

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   // 版本号
-  app_version: "v1.17.1",
+  app_version: "v1.17.2",
   // 小程序appid
   app_id: "wx5bd705b2bc91c73b",
   // EMAS空间ID
--- miniprogram/pages/info/devTeam/devTeam.js
+++ miniprogram/pages/info/devTeam/devTeam.js
@@ -10,6 +10,12 @@ Page({
     text_cfg: text_cfg,
     github_link: "https://github.com/sysucats/zhongdamaopu",
     update_log: [{
+      version: "v1.17.2",
+      content: [
+        "修复已知BUG（感谢LightHouse-1024）",
+      ],
+      time: "2025/11/02"
+    }, {
       version: "v1.17.1",
       content: [
         "修复已知BUG（感谢xuannfx）",
